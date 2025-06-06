80b90b1ba8ce6c3fbe997c4f4532d4c59a0af9d3
Nicholas Griffiths <nichg999@gmail.com>
2025-06-06 00:25:04 -0600

    factoring code for first question of trivia game

--- trivia.js
+++ trivia.js
@@ -7,10 +7,30 @@ const readlineSync = require('readline-sync');
 
 function greetUser() {
     console.log("Welcome to Trivia!");
-    readlineSync.keyInYN("Do you want to play? ");
+    const wantToPlay = readlineSync.keyInYN("Do you want to play? ");
+    return wantToPlay;
+  }
+
+// Ask a question and record the user's response
+// Check the user response against the correct answer
+// If the answer is correct, congratulate the user
+
+const firstQuestion = "What is the freezing point of water?";
+const firstQuestoinAnswers = ['0 C', '100 C', '32 C', '212 C'];
+const firstQuestionAnswer = answers[0]; // String
+
+function askQuestion(question, answers, correctAnswer) {
+  console.log("your first question is...");
+  const userResponse = readlineSync.keyInSelect(answers, 'Choose an option: ');
+  if (correctAnswer === answers[userResponse]) {
+    console.log("Congratulations! You got it right.");
+  } else {
+    console.log("Sorry, that's not correct. The correct answer is " + correctAnswer);
+  }
 }
 
 greetUser();
+askQuestion();
 
 
 
