f3d6cfb6e2d99d4315277fa8179402b02e34ea66
Nicholas Griffiths <nichg999@gmail.com>
2025-06-06 22:53:37 -0600

    changed how the code runs in the terminal

--- trivia.js
+++ trivia.js
@@ -16,11 +16,12 @@ function greetUser() {
 // If the answer is correct, congratulate the user
 
 const firstQuestion = "What is the freezing point of water?";
-const firstQuestoinAnswers = ['0 C', '100 C', '32 C', '212 C'];
-const firstQuestionAnswer = answers[0]; // String
+const firstQuestionAnswers = ['0 C', '100 C', '32 C', '212 C'];
+const correctAnswer = firstQuestionAnswers[0];
 
 function askQuestion(question, answers, correctAnswer) {
   console.log("your first question is...");
+  console.log(question);
   const userResponse = readlineSync.keyInSelect(answers, 'Choose an option: ');
   if (correctAnswer === answers[userResponse]) {
     console.log("Congratulations! You got it right.");
@@ -30,7 +31,7 @@ function askQuestion(question, answers, correctAnswer) {
 }
 
 greetUser();
-askQuestion();
+askQuestion(firstQuestion, firstQuestionAnswers, correctAnswer);
 
 
 
