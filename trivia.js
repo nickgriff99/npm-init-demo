7ea058127622eeb9116831bc386c095abe56e007
Nicholas Griffiths <nichg999@gmail.com>
2025-06-07 18:25:20 -0600

    refactored code to be more best practices

--- trivia.js
+++ trivia.js
@@ -5,37 +5,64 @@ const readlineSync = require('readline-sync');
 // Record the user's response
 
 
-function greetUser() {
-    console.log("Welcome to Trivia!");
-    const wantToPlay = readlineSync.keyInYN("Do you want to play? ");
-    return wantToPlay;
-  }
 
 // Ask a question and record the user's response
 // Check the user response against the correct answer
 // If the answer is correct, congratulate the user
 
-const firstQuestion = "What is the freezing point of water?";
-const firstQuestionAnswers = ['0 C', '100 C', '32 C', '212 C'];
-const correctAnswer = firstQuestionAnswers[0];
 
-const secondQuestion = "When did the founding fathers sign the Declaration of Independence?";
-const secondQuestionAnswers = ['1776', '1787', '1791', '1801'];
-const secondCorrectAnswer = secondQuestionAnswers[0];
+// Worse way to structure the questions and answers
+// const firstQuestion = "What is the freezing point of water?";
+// const firstQuestionAnswers = ['0 C', '100 C', '32 C', '212 C'];
+// const firstQuestionAnswerIndex = firstQuestionAnswers[0];
+
+// const secondQuestion = "When did the founding fathers sign the Declaration of Independence?";
+// const secondQuestionAnswers = ['1776', '1787', '1791', '1801'];
+// const secondCorrectAnswers = secondQuestionAnswers[0];
+
+
+
+// greetUser();
+// askQuestion(questions);
+// askQuestion(secondQuestion, secondQuestionAnswers, secondCorrectAnswers);
+
+function greetUser() {
+    console.log("Welcome to Trivia!");
+    const wantToPlay = readlineSync.keyInYN("Do you want to play? ");
+    return wantToPlay;
+}
+
+// Better way to structure the questions and answers
+const questions = [
+  {
+    question: "What is the freezing point of water?",
+    answers: ['0 C', '100 C', '32 C', '212 C'],
+    correctIndex: 0,
+  },
+  {
+    question: "When did the founding fathers sign the Declaration of Independence?",
+    answers: ['1776', '1787', '1791', '1801'],
+    correctIndex: 0,
+  },
+];
 
 function askQuestion(question, answers, correctAnswer) {
-  console.log("your first question is...");
+  console.log("Your question is...");
   console.log(question);
-  const userResponse = readlineSync.keyInSelect(answers, 'Choose an option: ');
+  const userResponse = readlineSync.keyInSelect(answers, 'Choose an option: ');  
   if (correctAnswer === answers[userResponse]) {
     console.log("Congratulations! You got it right.");
+    return true;
   } else {
     console.log("Sorry, that's not correct. The correct answer is " + correctAnswer);
+    return false;
   }
 }
 
-greetUser();
-askQuestion(firstQuestion, firstQuestionAnswers, correctAnswer);
-
-
-
+if (greetUser()) {
+  for (const q of questions) {
+    askQuestion(q.question, q.answers, q.answers[q.correctIndex]);
+  }
+} else {
+  console.log("Maybe next time!");
+}
